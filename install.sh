8463aea1b07ae7325a3f88d78a87246b3cf1bd03
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-08-23 16:32:40 +0000

    Increase swap partition size to 72GB

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ elif [ "$nvme_count" -eq 1 ]; then
     # Partition single disk: EFI + Swap + Root
     sgdisk -Z "$disk1"
     sgdisk -n 1:0:+2G -t 1:ef00 "$disk1"    # EFI partition
-    sgdisk -n 2:0:+8G -t 2:8200 "$disk1"    # Swap partition
+    sgdisk -n 2:0:+72G -t 2:8200 "$disk1"    # Swap partition
     sgdisk -n 3:0:0 -t 3:8300 "$disk1"      # Root partition
 
     # Create filesystems
@@ -152,7 +152,7 @@ elif [ "$nvme_count" -eq 2 ]; then
     mkfs.ext4 "${disk1}p2"
 
     # Partition secondary disk: Swap + Data
-    sgdisk -n 1:0:+8G -t 1:8200 "$disk2"      # Swap partition
+    sgdisk -n 1:0:+72G -t 1:8200 "$disk2"      # Swap partition
     sgdisk -n 2:0:0 -t 2:8300 "$disk2"        # Data partition
     mkswap --label diskswap "${disk2}p1"
     mkfs.ext4 "${disk2}p2"
