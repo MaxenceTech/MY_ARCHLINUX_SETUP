e05277a087f6719dcfe45a10465276cc882e850b
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-08-27 06:54:14 +0000

    Remove service restart from a-unplug.sh

--- SCRIPT/ACPID/SCRIPT/a-unplug.sh
+++ SCRIPT/ACPID/SCRIPT/a-unplug.sh
@@ -28,8 +28,5 @@ echo 22000 | tee /sys/class/backlight/intel_backlight/brightness
 # Wait for system to stabilize
 sleep 5
 
-# Deblock TDP after unplug-plug
-systemctl restart nvidia-powerd.service
-
 # Set power profile to power saver mode
 powerprofilesctl set power-saver
