d377e91d3268c31ff12c38d4dda49a67e0f23448
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-08-27 07:27:10 +0000

    Update power management settings in gamemode-unplug.sh

--- SCRIPT/ACPID/SCRIPT/gamemode-unplug.sh
+++ SCRIPT/ACPID/SCRIPT/gamemode-unplug.sh
@@ -9,20 +9,23 @@
 # Usage: Called automatically by ACPI handler when adapter is plugged in
 #==============================================================================
 
-# Configure MSI-EC for performance mode
-echo turbo | tee /sys/devices/platform/msi-ec/shift_mode
-echo auto | tee /sys/devices/platform/msi-ec/fan_mode
-
-# Set CPU governors to performance mode
-echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
-echo 0 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
-
-# Switch display to lower refresh rate for power saving
-runuser -l "$(last | cut -f 1 -d " " | sed '1p;d')" -c 'gnome-randr modify eDP-1 --mode 2560x1600@60.008'
+# Set power profile to performance mode
+powerprofilesctl set balanced
 
 # Wait for system to stabilize
 sleep 5
 
-# Set power profile to performance mode
-powerprofilesctl set performance
+#Disable turno boost
+echo 1 | tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+# Configure MSI-EC for performance mode
+echo comfort | tee /sys/devices/platform/msi-ec/shift_mode
+echo auto | tee /sys/devices/platform/msi-ec/fan_mode
+
+# Set CPU governors to performance mode
+echo performance| tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo balance_power | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+echo 6 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
+
+# Switch display to lower refresh rate for power saving
+runuser -l "$(last | cut -f 1 -d " " | sed '1p;d')" -c 'gnome-randr modify eDP-1 --mode 2560x1600@60.008'
