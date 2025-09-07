f10b24c22e698e05cca82ee1f8a7b41b7396e013
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-09-07 21:19:58 +0200

    Change display mode to 2560x1600@240.014

--- SCRIPT/ACPID/SCRIPT/a-plug.sh
+++ SCRIPT/ACPID/SCRIPT/a-plug.sh
@@ -28,7 +28,7 @@ echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_p
 echo 0 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
 
 # Configure display for high refresh rate
-runuser -l "$(last | cut -f 1 -d " " | sed '1p;d')" -c 'gnome-randr modify eDP-1 --mode 2560x1600@119.935'
+runuser -l "$(last | cut -f 1 -d " " | sed '1p;d')" -c 'gnome-randr modify eDP-1 --mode 2560x1600@240.014'
 
 # Restore saved brightness if available
 if [ -f /tmp/brightness-saved ]; then
