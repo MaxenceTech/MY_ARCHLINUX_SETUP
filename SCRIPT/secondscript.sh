9deb1e4e79506db8b4cb6fc8cadab10f6178a983
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-09-14 12:40:59 +0000

    Add NVIDIA hook for initcpio updates

--- SCRIPT/secondscript.sh
+++ SCRIPT/secondscript.sh
@@ -150,6 +150,24 @@ echo 'options nvidia "NVreg_DynamicPowerManagement=0x03" NVreg_UsePageAttributeT
 
 sudo mkinitcpio-editor -a nvidia nvidia_modeset nvidia_uvm nvidia_drm
 
+echo "[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+# You can remove package(s) that don't apply to your config, e.g. if you only use nvidia-open you can remove nvidia-lts as a Target
+Target=nvidia
+Target=nvidia-open
+# If running a different kernel, modify below to match
+Target=linux
+
+[Action]
+Description=Updating NVIDIA module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'" | sudo tee /etc/pacman.d/hooks/nvidia.hook
+
 sudo systemctl enable nvidia-powerd.service
 
 # Add OpenCL Support
