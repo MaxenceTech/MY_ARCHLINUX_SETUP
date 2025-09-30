31bf54844e2a0dac51e5ddb0bf8147ef66600623
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-09-30 11:33:19 +0000

    Update secure-boot.sh

--- SCRIPT/secure-boot.sh
+++ SCRIPT/secure-boot.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [ $(sudo sbctl status | grep "Setup Mode" | grep -c "Enabled") -gt 0 ]; then
 	sudo sbctl create-keys
 	sudo sbctl enroll-keys -m -f
-	sudo sbctl sign-all -g
+	sudo sbctl verify | sed -E 's|^.* (/.+) is not signed$|sudo sbctl sign -s "\1"|e'
 else
 	echo "Not in setup mode ! Exiting !"
 	exit 1
