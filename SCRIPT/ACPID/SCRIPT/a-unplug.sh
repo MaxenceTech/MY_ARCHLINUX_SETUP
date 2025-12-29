e9aeb1b4dd955496eb3b794fbc2bb85e38730b29
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2025-12-29 15:36:24 +0100

    Update a-unplug.sh

--- SCRIPT/ACPID/SCRIPT/a-unplug.sh
+++ SCRIPT/ACPID/SCRIPT/a-unplug.sh
@@ -12,17 +12,10 @@
 #Enable turno boost
 echo 0 | tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
-# Configure MSI-EC for power saving mode
-echo eco | tee /sys/devices/platform/msi-ec/shift_mode
-echo auto | tee /sys/devices/platform/msi-ec/fan_mode
-
 # Set CPU governors to power saving mode
 echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo power | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 echo 15 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
-
-# Switch display to lower refresh rate for power saving
-runuser -l "$(last | cut -f 1 -d " " | sed '1p;d')" -c 'gnome-randr modify $(gnome-randr | grep "eDP" | cut -f 1 -d " "  | sed -n '2p') --mode 2560x1600@60.008'
 
 # Save current brightness and reduce it for battery conservation
 tee /tmp/brightness-saved < /sys/class/backlight/intel_backlight/brightness
@@ -30,9 +23,6 @@ echo 22000 | tee /sys/class/backlight/intel_backlight/brightness
 
 # Wait for system to stabilize
 sleep 5
-
-# Deblock TDP after unplug-plug
-systemctl restart nvidia-powerd.service
 
 # Set power profile to power saver mode
 powerprofilesctl set power-saver
