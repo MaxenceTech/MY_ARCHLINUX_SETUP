5b2a6d011d9364bbfefc82b9df8f67a4c9dffebf
MaxenceTech <69858379+MaxenceTech@users.noreply.github.com>
2026-01-20 10:19:41 +0100

    Remove USBGuard setup from secondscript.sh
    Removed USBGuard installation and configuration steps.

--- SCRIPT/secondscript.sh
+++ SCRIPT/secondscript.sh
@@ -692,32 +692,6 @@ sudo fc-cache-32 --force
 sudo passwd --lock root
 sudo sed -i.bak '0,/^auth/s/^auth/auth optional pam_faildelay.so delay=4000000\n&/' /etc/pam.d/system-login
 
-sudo pacman -S usbguard --noconfirm
-sudo sed -i "s/^IPCAllowedUsers=root/& $USER/" /etc/usbguard/usbguard-daemon.conf
-sudo sed -i 's/^PresentControllerPolicy=.*/PresentControllerPolicy=apply-policy/' /etc/usbguard/usbguard-daemon.conf
-sudo usbguard generate-policy | sudo tee -a /etc/usbguard/rules.conf
-sudo systemctl enable --now usbguard-dbus.service
-
-sudo tee /etc/polkit-1/rules.d/70-allow-usbguard.rules << 'EOF'
-// Allow users in wheel group to communicate with USBGuard
-polkit.addRule(function(action, subject) {
-    if ((action.id == "org.usbguard.Policy1.listRules" ||
-         action.id == "org.usbguard.Policy1.appendRule" ||
-         action.id == "org.usbguard.Policy1.removeRule" ||
-         action.id == "org.usbguard.Devices1.applyDevicePolicy" ||
-         action.id == "org.usbguard.Devices1.listDevices" ||
-         action.id == "org.usbguard1.getParameter" ||
-         action.id == "org.usbguard1.setParameter") &&
-        subject.active == true && subject.local == true &&
-        subject.isInGroup("wheel")) {
-            return polkit.Result.YES;
-    }
-});
-EOF
-
-gsettings set org.gnome.desktop.privacy usb-protection true
-gsettings set org.gnome.desktop.privacy usb-protection-level always
-
 sudo mkinitcpio -p linux
 
 if [ "$yayerror" -eq 0 ]; then
